8c31a0621900ff5c002812f024c8abf3211dfd47
Gabriel Cardoso <gabriel.cardoso010@gmail.com>
2017-12-17 19:21:34 -0200

    Teste

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestConctUrl(t *testing.T) {
 	cases := []struct {
-		in struct { user, pass, senha string}, 
+		in   struct{ user, pass, senha string }
 		want string
 	}{
 		{{"root", "root", "localhost.com"}, "http://root:root@localhost.com"},
